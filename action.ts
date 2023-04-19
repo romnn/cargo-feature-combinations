093a11b148c020f4c5e1a331a44d939ac26e6e9d
romnnn <roman.dahm@gmx.de>
2023-04-19 02:04:16 +0200

    add action

---
+++ action.ts
@@ -0,0 +1,66 @@
+import * as core from "@actions/core";
+import * as exec from "@actions/exec";
+import {
+  parseCargoPackageManifestAsync,
+  Repo,
+  RustTarget,
+} from "action-get-release";
+import * as path from "path";
+
+async function getVersion(): Promise<string> {
+  let version = "latest";
+  const manifest = await parseCargoPackageManifestAsync(
+    path.join(__dirname, "../Cargo.toml")
+  );
+  let manifestVersion = manifest.package.version;
+  if (manifestVersion && manifestVersion !== "") {
+    version = `v${manifestVersion}`;
+  }
+  let versionOverride = core.getInput("version");
+  if (versionOverride && versionOverride !== "") {
+    version = versionOverride;
+  }
+  return version;
+}
+
+async function run(): Promise<void> {
+  const repo = new Repo();
+  const version = await getVersion();
+  core.debug(`version=${version}`);
+
+  let release;
+  try {
+    release =
+      version === "" || version === "latest"
+        ? await repo.getLatestRelease()
+        : await repo.getReleaseByTag(version);
+  } catch (err: unknown) {
+    throw new Error(
+      `failed to fetch ${version} release for ${repo.fullName()}: ${err}`
+    );
+  }
+  core.debug(
+    `found ${
+      release.assets().length
+    } assets for ${version} release of ${repo.fullName()}`
+  );
+
+  const { platform, arch } = new RustTarget();
+  core.debug(`host system: platform=${platform} arch=${arch}`);
+
+  // publish-crates-action-x86_64-unknown-linux-gnu.tar.gz
+  const asset = `publish-crates-action-${arch}-unknown-${platform}-gnu.tar.gz`;
+
+  let downloaded;
+  try {
+    downloaded = await release.downloadAsset(asset, { cache: false });
+  } catch (err: unknown) {
+    throw new Error(`failed to download asset ${asset}: ${err}`);
+  }
+
+  // core.addPath(downloaded);
+  const executable = path.join(downloaded, "publish-crates-action");
+  await exec.exec(executable);
+}
+
+run().catch((error) => core.setFailed(error.message));
